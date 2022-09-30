b370d322f5778c3dfde96830428f80faf6cd3a77
adriabalmes <aberruez@jaumebalmes.net>
2022-09-30 17:26:07 +0200

    actualizacion del archivo

--- Git.java
+++ Git.java
@@ -15,7 +15,7 @@ public class Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HolaMon");
+        System.out.println("cambioHecho");
+        System.out.println("nuevaOrden");
     }
-    
 }
